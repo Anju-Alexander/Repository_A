c07086423ac5882c616daf4f1bce8f5935987fbe
Anju-Alexander <anjusara102@gmail.com>
2023-05-25 11:20:29 +0530

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+import org.beginsecure.domain.primitives.Log4j;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        Log4j obj = new Log4j();
+        obj.disp();
+        System.out.println("Hello world!");
+    }
+}
